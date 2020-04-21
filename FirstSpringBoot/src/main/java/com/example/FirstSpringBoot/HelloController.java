e774d1e41620296d5afad94f9e93894f30a9feb6
vilasvarghese <vilas.varghese@gmail.com>
2020-04-21 12:18:05 +0530

    spring boot projects

---
+++ FirstSpringBoot/src/main/java/com/example/FirstSpringBoot/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.FirstSpringBoot;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@RequestMapping("/hello")
+	public String sayHello() {
+	//Question: is it mandatory that mapping methods should return String?
+		return "Hello from Vilas";
+	}
+}
